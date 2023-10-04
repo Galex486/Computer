1f713a2cdb7de3ce72df40f579e413083187f1b3
Alexandru Grama <alexandru.grama@nsih.local>
2023-10-04 13:58:45 +0300

    Initial commit

---
+++ src/edu/tekwill/academy/computer/Computer.java
@@ -0,0 +1,13 @@
+import edu.tekwill.academy.computer.components.*;
+import edu.tekwill.academy.computer.peripherals.*;
+
+public class Computer {
+    HardDrive hardrive;
+    Motherboard motherboard;
+    Processor procesor;
+    Keyboard keyboard;
+    Monitor monitor;
+    Mouse mouse;
+
+
+}
